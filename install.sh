acbda4ed2591133a2475d70dbf52417ee931ffda
Greg Lund-Chaix <gchaix@squishymedia.com>
2012-11-08 14:38:59 -0800

    add vbox update

--- install.sh
+++ install.sh
@@ -6,3 +6,12 @@ echo 0 >/selinux/enforce
 echo "Enabling EPEL:"
 su -c 'rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-7.noarch.rpm'
 #yum -y update
+
+# Installing the virtualbox guest additions
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm VBoxGuestAdditions_$VBOX_VERSION.iso
