72507b1e285e09d5d515bf524ed7840655fef4aa
Greg Lund-Chaix <gchaix@squishymedia.com>
2012-11-08 08:37:29 -0800

    fix db IP, install drush

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 # disable selinux
 echo 0 >/selinux/enforce
 
+echo "Enabling EPEL:"
 su -c 'rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-7.noarch.rpm'
 yum -y update
+
+echo "\n Installing drush:"
+pear channel-discover pear.drush.org
+pear install drush/drush
 
