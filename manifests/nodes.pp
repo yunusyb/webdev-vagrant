6f39ffbcda3da5762f63a9ff6ae5f8548e618988
Greg Lund-Chaix <gchaix@squishymedia.com>
2012-12-13 11:17:17 -0800

    squishy-config.pp is now obsolete and part of nodes.pp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,6 +2,50 @@ node /^web\d+/ {
 	include apache
 	include php::mod_php5
 	php::module { [ 'pecl-apc', 'pecl-memcached', 'pdo', 'gd', 'mbstring', 'xml' ]: }
+    # mysql-client configs
+    file { 'my.cnf':
+		path   => '/home/vagrant/.my.cnf',
+		ensure => file,
+		mode   => 0644,
+		source => '/vagrant/files/my.cnf',
+	}
+	# Apache config
+	firewall { "000 allow http":
+		proto  => 'tcp',
+		port   => '80',
+		action => 'accept',
+	}
+	file { '/server':
+		ensure => directory,
+		mode   => 2775,
+		owner  => 'vagrant',
+		group  => 'vagrant',
+	}
+	file { '/server/htdocs':
+		ensure => directory,
+		mode   => 2775,
+		owner  => 'vagrant',
+		group  => 'vagrant',
+	}
+	file { '/server/htdocs/index.php':
+		ensure => file,
+		mode   => 0644,
+		source => '/vagrant/files/index.php',
+		owner  => 'vagrant',
+		group  => 'vagrant',
+	}
+
+	# PHP config
+	php::ini { '/etc/php.ini':
+		display_errors => 'On',
+		memory_limit   => '256M',
+	}
+	php::module::ini { 'pecl-apc':
+	    settings           => {
+	        'apc.enabled'  => '1',
+	        'apc.shm_size' => '128m',
+	    }
+	}
 	include git
 }
 
@@ -17,6 +61,11 @@ node /^database\d+/ {
 	    host     => 'localhost',
 	    grant    => ['all'],
 	}
+    firewall { "000 allow mysql":
+        proto  => 'tcp',
+        port   => '3306',
+        action => 'accept',
+    }
 	include git
 }
 
