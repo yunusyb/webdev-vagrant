6f8b5a33da6842537324802d26bad0c61c907b81
Greg Lund-Chaix <gchaix@squishymedia.com>
2013-06-12 16:12:17 -0700

    Clean out old-style setup, convert to new puppetized

--- modules/drush/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-# Download Drush from PECL
-
-class drush {
-    exec { "drush":
-        command => "/vagrant/modules/drush/files/drush-install.sh",
-        creates => "/usr/bin/drush",
-    }
-}
-
