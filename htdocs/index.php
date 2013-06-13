ceb6914a5f9a96d74921abd3dfe52bdb06e814ef
Greg Lund-Chaix <gchaix@squishymedia.com>
2013-06-13 14:56:12 -0700

    Show server IP on default index

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,9 +2,7 @@
 <head><title>Hello world</title></head>
 <body>
 <?php
-echo $_SERVER['SERVER_NAME'];
-echo ' - ';
-echo date(DATE_RFC822);
+echo $_SERVER['SERVER_NAME'] . ' - '. $_SERVER['SERVER_ADDR'] . ' - ' . date(DATE_RFC822);
 ?>
 </body>
 </html>
