142a6edb2076d68802122ce561358a6d6fcf958e
Evan Heidtmann <evan.heidtmann@gmail.com>
2013-06-14 16:28:02 -0700

    Add phpinfo in htdocs

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,8 +1,11 @@
 <html>
 <head><title>Hello world</title></head>
 <body>
+<div>
 <?php
 echo $_SERVER['SERVER_NAME'] . ' - '. $_SERVER['SERVER_ADDR'] . ' - ' . date(DATE_RFC822);
 ?>
+</div>
+<a href="phpinfo.php">phpinfo</a>
 </body>
 </html>
