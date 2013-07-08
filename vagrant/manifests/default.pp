b8e4483605a325bc139d676c5e883ea77da3e4f5
Greg Lund-Chaix <gchaix@squishymedia.com>
2013-07-08 15:59:13 -0700

    clean up vhost definition, add security

--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -2,12 +2,20 @@ $mysql_root_password = 'root'
 
 include squishy_config::minimum
 include squishy_config::lamp
+include squishy_config::mysql
 include squishy_config::drupal
 
 apache::vhost { 'vagrant':
-  priority => '10',
-  port => '80',
-  docroot => '/server/htdocs',
+  priority    => '10',
+  port        => '80',
+  docroot     => '/server/htdocs'
+  override    => ['all'],
+  directories => [ { path => [ '/server/htdocs', allow_override => ['All'] ] }, ]
+  custom_fragment => "<Directory ~ 'sites/[^/]+/files/'>
+    AllowOverride None
+    #Prevent executable PHP in uploaded files
+    php_admin_flag engine off
+  </Directory>",
 }
 
 # this directive ensures that apache::vhost doesn't clobber our docroot.
